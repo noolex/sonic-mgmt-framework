0ce770582f4af6de50029bb680cf8b66346b08bf
Marimuthu Sakthivel <marimuthu.sakthivel@dell.com>
2020-03-16 07:06:40 -0700

    Support for container/list top level delete for VLAN, port-channel, IP address, un-numbered and NAT features.

--- src/translib/transformer/xfmr_vxlan_intf.go
+++ src/translib/transformer/xfmr_vxlan_intf.go
@@ -748,7 +748,6 @@ func getVxlanNiUriPath(uri string) (*gnmipb.Path, error) {
 
 var YangToDb_intf_vxlan_config_xfmr SubTreeXfmrYangToDb = func(inParams XfmrParams) (map[string]map[string]db.Value, error) {
 	var err error
-
 	if log.V(3) {
 		log.Info("YangToDb_intf_vxlan_config_xfmr entering => inParams.uri => ", inParams.uri)
 	}
