414107eb606b4d16d097146512a607fd248ada56
Amruta Sali <amruta_sali@dell.com>
2020-04-17 15:32:01 -0700

    Leaf-list sonic yang UT cases

--- src/translib/transformer/xfmr_leaf_list_test.go
+++ src/translib/transformer/xfmr_leaf_list_test.go
@@ -48,3 +48,239 @@ func Test_LeafList(t *testing.T) {
 	unloadConfigDB(rclient, snmp_vacm_view_map)
 }
 
+
+func Test_LeafList_DataTypeConversion_SonicYang(t *testing.T) {
+        fmt.Println("\n\n+++++++++++++ Performing Leaf-list Data-Type Conversion getting called Sonic Yang Cases ++++++++++++")
+        var prereq_map map[string]interface{}
+        var expected_map map[string]interface{}
+        var url, url_body_json, expected_get_json string
+
+	/** Data-type conversion getting called on leaf-list create**/
+        prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        unloadConfigDB(rclient, prereq_map)
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]"
+        url_body_json = "{\"sonic-acl:stage\":\"INGRESS\",\"sonic-acl:type\":\"L3\",\"sonic-acl:ports\":[\"Ethernet0\"]}"
+        t.Run("Data-type conversion create.", processSetRequest(url, url_body_json, "POST", false, nil))
+        time.Sleep(1 * time.Second)
+        t.Run("Verify Data-type conversion create.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, prereq_map)
+        /*********************/
+
+
+        /** Data-type conversion getting called on leaf-list Update**/
+        prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0,Ethernet8",
+										          "stage": "INGRESS",
+										          "type": "L3"}}}
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0,Ethernet8,Ethernet124",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        loadConfigDB(rclient, prereq_map)
+	url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports"
+        url_body_json = "{\"sonic-acl:ports\":[\"Ethernet0\",\"Ethernet124\",\"Ethernet8\"]}"
+        t.Run("Data-type conversion update.", processSetRequest(url, url_body_json, "PATCH", false, nil))
+        time.Sleep(1 * time.Second)
+        t.Run("Verify Data-type conversion update.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, prereq_map)
+        /*********************/
+
+        /** Data-type conversion getting called on leaf-list delete **/
+        prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0,Ethernet8,Ethernet124",
+										          "stage": "INGRESS",
+										          "type": "L3"}}}
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0,Ethernet124",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        loadConfigDB(rclient, prereq_map)
+	url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports[ports=Ethernet8]"
+        t.Run("Data-type conversion delete.", processDeleteRequest(url, false))
+        time.Sleep(1 * time.Second)
+        t.Run("Verify Data-type conversion delete.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, prereq_map)
+        /*********************/
+
+        /** Data-type conversion getting called on leaf-list get**/
+        prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet8,Ethernet16",
+										          "stage": "INGRESS",
+										          "type": "L3"}}}
+
+	url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports"
+        expected_get_json = "{\"sonic-acl:ports\":[\"Ethernet8\",\"Ethernet16\"]}"
+        loadConfigDB(rclient, prereq_map)
+        t.Run("FieldXfmr leaf-list get.", processGetRequest(url, expected_get_json, false))
+        time.Sleep(1 * time.Second)
+        unloadConfigDB(rclient, prereq_map)
+        /*********************/
+
+        fmt.Println("\n\n+++++++++++++ Done Performing Leaf-list Data-Type Conversion getting called Sonic Yang Cases ++++++++++++")
+}
+
+func Test_LeafList_CRU_SonicYang(t *testing.T) {
+        fmt.Println("\n\n+++++++++++++ Performing Leaf-list CRU Sonic Yang Cases ++++++++++++")
+        var prereq_map map[string]interface{}
+        var expected_map map[string]interface{}
+        var url, url_body_json string
+
+        /** sonic yang leaf-list create**/
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        unloadConfigDB(rclient, expected_map)
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]"
+        url_body_json = "{\"sonic-acl:stage\":\"INGRESS\",\"sonic-acl:type\":\"L3\",\"sonic-acl:ports\":[\"Ethernet0\"]}"
+        t.Run("leaf-list create.", processSetRequest(url, url_body_json, "POST", false, nil))
+        time.Sleep(1 * time.Second)
+        t.Run("Verify leaf-list create.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, expected_map)
+        /*********************/
+
+
+        /**  sonic yang leaf-list Update/merge**/
+        prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0,Ethernet8",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0,Ethernet8,Ethernet124",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        loadConfigDB(rclient, prereq_map)
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports"
+        url_body_json = "{\"sonic-acl:ports\":[\"Ethernet0\",\"Ethernet124\",\"Ethernet8\"]}"
+        t.Run("leaf-list update/merge.", processSetRequest(url, url_body_json, "PATCH", false, nil))
+        t.Run("Verify leaf-list update/merge.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, prereq_map)
+
+        fmt.Println("\n\n+++++++++++++ Done Performing Leaf-list CRU Sonic Yang Cases ++++++++++++")
+}
+
+func Test_LeafList_Delete_SonicYang(t *testing.T) {
+        fmt.Println("\n\n+++++++++++++ Performing Leaf-list Deletion Sonic Yang Cases ++++++++++++")
+        var prereq_map map[string]interface{}
+        var expected_map map[string]interface{}
+        var url string
+
+        /** delete specific item from leaf-list **/
+        prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0,Ethernet8,Ethernet124",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet0,Ethernet124",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        loadConfigDB(rclient, prereq_map)
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports[ports=Ethernet8]"
+        t.Run("Delete an item in leaf-list.", processDeleteRequest(url, false, nil))
+        time.Sleep(1 * time.Second)
+        t.Run("Delete an item in leaf-list.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, prereq_map)
+	/***********************************/
+
+	/** Delete specific item from leaf-list, when leaf-list does not exist in DB **/
+	prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "type": "L3"}}}
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "type": "L3"}}}
+        loadConfigDB(rclient, prereq_map)
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports[ports=Ethernet8]"
+
+        t.Run("Delete an item when leaf-list doesn't exist in DB.", processDeleteRequest(url, false, nil))
+        time.Sleep(1 * time.Second)
+        t.Run("Verify Delete an item when leaf-list doesn't exist in DB.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, prereq_map)
+        /*********************/
+
+        /** delete an item from leaf-list which happens to be the only element in leaf-lis in DB. **/
+	prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet8",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        loadConfigDB(rclient, prereq_map)
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports[ports=Ethernet8]"
+        t.Run("Delete an item which is the only one in leaf-list.", processDeleteRequest(url, false, nil))
+        time.Sleep(1 * time.Second)
+        t.Run("Verify Delete an item which is the only one in leaf-list.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, prereq_map)
+        /*********************/
+
+        /** delete entire leaf-list. **/
+	prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "Ethernet8",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        loadConfigDB(rclient, prereq_map)
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports"
+        t.Run("Delete entire leaf-list.", processDeleteRequest(url, false, nil))
+        time.Sleep(1 * time.Second)
+        t.Run("Delete entire leaf-list.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, prereq_map)
+        /*********************/
+
+        /** delete entire leaf-list, when leaf-list doesn't exist in DB. **/
+	prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "type": "L3"}}}
+        expected_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "type": "L3"}}}
+        loadConfigDB(rclient, prereq_map)
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports"
+        t.Run("Delete leaf-list not there in DB.", processDeleteRequest(url, false, nil))
+        time.Sleep(1 * time.Second)
+        t.Run("Delete leaf-list not there in DB.", verifyDbResult(rclient, "ACL_TABLE|MyACL1_ACL_IPV4", expected_map, false))
+        unloadConfigDB(rclient, prereq_map)
+
+        fmt.Println("+++++++++++++ Done!!! Performing Leaf-list Deletion Sonic Yang Cases ++++++++++++")
+}
+
+func Test_LeafList_Get_SonicYang(t *testing.T) {
+        fmt.Println("\n\n+++++++++++++ Performing Leaf-list Get Sonic Yang Cases ++++++++++++")
+        var prereq_map map[string]interface{}
+        var url, expected_get_json string
+
+        /** leaf-list get when leaf-list doesn't exist in DB(valid field in yang)**/
+        prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports"
+        expected_get_json = "{}"
+        loadConfigDB(rclient, prereq_map)
+        t.Run("Get leaf-list not there in DB.", processGetRequest(url, expected_get_json, false))
+        time.Sleep(1 * time.Second)
+        unloadConfigDB(rclient, prereq_map)
+        /*********************/
+
+        /** empty-string leaf-list get**/
+        prereq_map = map[string]interface{}{"ACL_TABLE":map[string]interface{}{"MyACL1_ACL_IPV4":map[string]interface{}{
+                                                                                          "ports@": "",
+                                                                                          "stage": "INGRESS",
+                                                                                          "type": "L3"}}}
+        url = "/sonic-acl:sonic-acl/ACL_TABLE/ACL_TABLE_LIST[aclname=MyACL1_ACL_IPV4]/ports"
+        expected_get_json = "{}"
+        loadConfigDB(rclient, prereq_map)
+        t.Run("Get leaf-list empty string in DB.", processGetRequest(url, expected_get_json, false))
+        time.Sleep(1 * time.Second)
+        unloadConfigDB(rclient, prereq_map)
+        /*********************/
+
+        fmt.Println("\n\n+++++++++++++ Done Performing Leaf-list Get Sonic Yang Cases ++++++++++++")
+}
+
