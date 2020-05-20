7fe2ee3560e2e568ed4aa6591d009076a7fc9173
Ashok Daparthi <Ashok_Daparthi@Dell.com>
2020-05-20 02:47:59 -0700

    Qos Mgmt CLI:DSCP to TC map implementation & Scheduler Policy changes

--- CLI/actioner/sonic_cli_qos_scheduler.py
+++ CLI/actioner/sonic_cli_qos_scheduler.py
@@ -10,8 +10,11 @@ from rpipe_utils import pipestr
 
 def invoke(func, args=[]):
     api = cc.ApiClient()
+    if func == 'get_openconfig_qos_qos_scheduler_policies':
+        path = cc.Path('/restconf/data/openconfig-qos:qos/scheduler-policies')
+        return api.get(path)
     if func == 'get_openconfig_qos_qos_scheduler_policies_scheduler_policy_schedulers':
-        path = cc.Path('/restconf/data/openconfig-qos:qos/scheduler-policies/scheduler-policy={name}/schedulers', name=args[0] )
+        path = cc.Path('/restconf/data/openconfig-qos:qos/scheduler-policies/scheduler-policy={name}/schedulers', name=args[0])
         return api.get(path)
     if func == 'patch_openconfig_qos_qos_scheduler_policies_scheduler_policy':
         path = cc.Path('/restconf/data/openconfig-qos:qos/scheduler-policies/scheduler-policy={name}/config', name=args[0] )
@@ -42,14 +45,18 @@ def invoke(func, args=[]):
         body = {"openconfig-qos-ext:weight": int(args[2])}
         return api.patch(path, body)
 
-    if func == 'patch_openconfig_qos_qos_scheduler_policies_scheduler_policy_schedulers_scheduler_two_rate_three_color_config_cir':
+    if func == 'patch_openconfig_qos_qos_scheduler_policies_scheduler_policy_schedulers_scheduler_two_rate_three_color_config_cir':    
+        cir_val = int(args[2]) * 1000
+        cir_str = str(cir_val)
         path = cc.Path('/restconf/data/openconfig-qos:qos/scheduler-policies/scheduler-policy={name}/schedulers/scheduler={sequence}/two-rate-three-color/config/cir', name=args[0], sequence=args[1])
-        body = {"openconfig-qos:cir": args[2]}
+        body = {"openconfig-qos:cir": cir_str}
         return api.patch(path, body)
 
     if func == 'patch_openconfig_qos_qos_scheduler_policies_scheduler_policy_schedulers_scheduler_two_rate_three_color_config_pir':
+        pir_val = int(args[2]) * 1000
+        pir_str = str(pir_val)
         path = cc.Path('/restconf/data/openconfig-qos:qos/scheduler-policies/scheduler-policy={name}/schedulers/scheduler={sequence}/two-rate-three-color/config/pir', name=args[0], sequence=args[1])
-        body = {"openconfig-qos:pir": args[2]}
+        body = {"openconfig-qos:pir": pir_str}
         return api.patch(path, body)
 
     if func == 'patch_openconfig_qos_qos_scheduler_policies_scheduler_policy_schedulers_scheduler_two_rate_three_color_config_bc':
@@ -61,6 +68,9 @@ def invoke(func, args=[]):
         path = cc.Path('/restconf/data/openconfig-qos:qos/scheduler-policies/scheduler-policy={name}/schedulers/scheduler={sequence}/two-rate-three-color/config/be', name=args[0], sequence=args[1])
         body = {"openconfig-qos:be": int(args[2])}
         return api.patch(path, body)
+    if func == 'delete_openconfig_qos_qos_scheduler_policies_scheduler_policy_schedulers_scheduler_config_priority':
+        path = cc.Path('/restconf/data/openconfig-qos:qos/scheduler-policies/scheduler-policy={name}/schedulers/scheduler={sequence}/config/priority', name=args[0], sequence=args[1])
+        return api.delete(path)
 
     return api.cli_not_implemented(func)
 
@@ -72,12 +82,14 @@ def run(func, args):
     if response.ok():
         if response.content is not None:
             api_response = response.content
-            
+
             #print api_response
-
             if func == 'get_openconfig_qos_qos_scheduler_policies_scheduler_policy_schedulers':
-                show_cli_output('show_qos_scheduler.j2', response)
-
+                show_cli_output(args[1], api_response)
+            elif func == 'get_openconfig_qos_qos_scheduler_policies':
+                show_cli_output(args[0], api_response)
+            else:
+                pass
     else:
         print response.error_message()
 
