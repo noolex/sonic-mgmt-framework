f3f234f7d6db44a4f11a46831728515c44bbdebe
Srinadh Penugonda <Srinadh_Penugondaa@dell.com>
2020-06-01 20:50:17 -0700

    Show audit log commnad implementation

--- CLI/actioner/sonic-cli-audit-log-rpc.py
+++ CLI/actioner/sonic-cli-audit-log-rpc.py
@@ -2,48 +2,22 @@ from collections import OrderedDict
 import sys
 import json
 from rpipe_utils import pipestr
-import cli_client as cc
 from scripts.render_cli import show_cli_output
-
-ShowOpts = [ "summary", "bgp", "connected", "static" ]
-def invoke_api(func, args):
-    body = None
-    api = cc.ApiClient()
-    response = "May 28 19:16:01.418649 sonic INFO sshd[30598]: Accepted password for admin from 10.14.8.140 port 43486 ssh2\n
-    May 28 19:16:01.606913 sonic INFO sshd[30598]: pam_unix(sshd:session): session opened for user admin by (uid=0)\n
-    "
-    return response
+import cli_client as cc
 
 def run(func, args):
-    try:
-        response = invoke_api(func, args)
-        if not response.ok():
-            #error response
-            print response.error_message()
-            exit(1)
-        print(response.content)
-
-#        d = response.content['sonic-ip-show:output']['response']
-#        if len(d) != 0 and "warning" not in d:
-#           d = json.loads(d)
-#
-#           if func == 'show_ip_route_summary':
-#               if len(args) >= 6 and args[4] == 'vrf' and args[5] != 'default':
-#                    d['vrfname'] = args[5]
-#               show_cli_output(args[0], d)
-#               return
-#
-#           routes = d
-#           keys = sorted(routes,key=getPrefixAndLen)
-#           temp = OrderedDict()
-#           for key in keys:
-#               temp[key] = routes[key]
-#           d = temp
-#           show_cli_output(args[0], d)
-
-    except:
-        # system/network error
-        print "%Error: Transaction Failure"
+    aa = cc.ApiClient()
+    if func == 'rpc_sonic_auditlog_show_auditlog':
+        keypath = cc.Path('/restconf/operations/sonic-auditlog:show-auditlog')
+        body = { "sonic-auditlog:input": {} }
+        response = aa.post(keypath, body)
+        if response.ok():
+            print("AUDIT LOG")
+            print(response.content["sonic-show-auditlog:output"]["audit-content"])
+        else:
+            print "%Error: Transaction Failure "
+            print(response.error_message())
+            print(response.status_code)
 
 if __name__ == '__main__':
 
@@ -51,3 +25,4 @@ if __name__ == '__main__':
     func = sys.argv[1]
 
     run(func, sys.argv[2:])
+
