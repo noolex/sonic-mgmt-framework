cf3880a9e745ada8d7ba8054cc8b25bbc42ffa31
Ashok Daparthi <Ashok_Daparthi@Dell.com>
2020-06-11 04:32:36 -0700

    QOS Mgmt: Maps, PFC

--- CLI/actioner/sonic_cli_qos_map.py
+++ CLI/actioner/sonic_cli_qos_map.py
@@ -26,6 +26,22 @@ def invoke(func, args=[]):
         path = cc.Path('/restconf/data/openconfig-qos:qos/openconfig-qos-maps-ext:dot1p-maps/dot1p-map={name}', name=args[0])
         return api.get(path)
 
+    if func == 'get_openconfig_qos_maps_ext_qos_tc_queue_maps' :
+        path = cc.Path('/restconf/data/openconfig-qos:qos/openconfig-qos-maps-ext:forwarding-group-queue-maps')
+        return api.get(path)
+
+    if func == 'get_openconfig_qos_maps_ext_qos_tc_queue_maps_tc_queue_map' :
+        path = cc.Path('/restconf/data/openconfig-qos:qos/openconfig-qos-maps-ext:forwarding-group-queue-maps/forwarding-group-queue-map={name}', name=args[0])
+        return api.get(path)
+
+    if func == 'get_openconfig_qos_maps_ext_qos_tc_pg_maps' :
+        path = cc.Path('/restconf/data/openconfig-qos:qos/openconfig-qos-maps-ext:forwarding-group-priority-group-maps')
+        return api.get(path)
+
+    if func == 'get_openconfig_qos_maps_ext_qos_tc_pg_maps_tc_pg_map' :
+        path = cc.Path('/restconf/data/openconfig-qos:qos/openconfig-qos-maps-ext:forwarding-group-priority-group-maps/forwarding-group-priority-group-map={name}', name=args[0])
+        return api.get(path)
+
     return api.cli_not_implemented(func)
 
 def run(func, args):
@@ -47,6 +63,14 @@ def run(func, args):
                 elif func == 'get_openconfig_qos_maps_ext_qos_dot1p_maps_dot1p_map':
                      show_cli_output(args[1], api_response)
                 elif func == 'get_openconfig_qos_maps_ext_qos_dot1p_maps':
+                     show_cli_output(args[0], api_response)
+                elif func == 'get_openconfig_qos_maps_ext_qos_tc_queue_maps_tc_queue_map':
+                     show_cli_output(args[1], api_response)
+                elif func == 'get_openconfig_qos_maps_ext_qos_tc_queue_maps':
+                     show_cli_output(args[0], api_response)
+                elif func == 'get_openconfig_qos_maps_ext_qos_tc_pg_maps_tc_pg_map':
+                     show_cli_output(args[1], api_response)
+                elif func == 'get_openconfig_qos_maps_ext_qos_tc_pg_maps':
                      show_cli_output(args[0], api_response)
 
        else:
