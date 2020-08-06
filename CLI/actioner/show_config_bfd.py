d9a5e1fad3608d6e56f0a0d9ecbe1c12c4da1da3
SumitAgarwal123 <sumit.agarwal@broadcom.com>
2020-08-05 23:41:14 -0700

    [JIRA SONIC-26812] Fixing show running
    Change-Id: If6ad4a6a4002a651f841e6345f744c39976b0748

--- CLI/actioner/show_config_bfd.py
+++ CLI/actioner/show_config_bfd.py
@@ -21,7 +21,7 @@ def bfd_get_key_values(in_map) :
         return False
     return True, in_map["remote-address"], in_map["interface"], in_map["vrf"], in_map["local-address"]
 
-def bfd_generate_command(in_map, key_name, cmd_prefix, match_value={}, cmd_suffix=' ;') :
+def bfd_generate_command(in_map, key_name, cmd_prefix, match_value={}, cmd_suffix=' ;', default='') :
     cmd_str = ''
 
     if in_map == None :
@@ -39,26 +39,31 @@ def bfd_generate_command(in_map, key_name, cmd_prefix, match_value={}, cmd_suffi
                         cmd_str = '{} {}'.format(cmd_prefix, cmd_suffix)
         else :
             value = in_map[key_name]
-            cmd_str = '{} {} {}'.format(cmd_prefix, value, cmd_suffix)
+            if ((default != '') and (value != default)):
+                cmd_str = '{} {} {}'.format(cmd_prefix, value, cmd_suffix)
 
     return cmd_str
 
 def bfd_generate_peer_view_cmd(rec, cmd_prefix):
     cmd_str = ''
 
-    cmd_str += bfd_generate_command(rec, 'detection-multiplier', cmd_prefix + 'detect-multiplier')
+    default_value = 3
+    cmd_str += bfd_generate_command(rec, 'detection-multiplier', cmd_prefix + 'detect-multiplier', default=default_value)
 
-    cmd_str += bfd_generate_command(rec, 'desired-minimum-echo-receive', cmd_prefix + 'echo-interval')
+    default_value = 50
+    cmd_str += bfd_generate_command(rec, 'desired-minimum-echo-receive', cmd_prefix + 'echo-interval', default=default_value)
 
     match_value = { True : '' }
     cmd_str += bfd_generate_command(rec, 'echo-active', cmd_prefix + 'echo-mode', match_value=match_value)
 
-    cmd_str += bfd_generate_command(rec, 'required-minimum-receive', cmd_prefix + 'receive-interval')
+    default_value = 300
+    cmd_str += bfd_generate_command(rec, 'required-minimum-receive', cmd_prefix + 'receive-interval', default=default_value)
 
     match_value = { False : '' }
     cmd_str += bfd_generate_command(rec, 'enabled', cmd_prefix + 'shutdown', match_value=match_value)
 
-    cmd_str += bfd_generate_command(rec, 'desired-minimum-tx-interval', cmd_prefix + 'transmit-interval')
+    default_value = 300
+    cmd_str += bfd_generate_command(rec, 'desired-minimum-tx-interval', cmd_prefix + 'transmit-interval', default=default_value)
 
     return cmd_str
 
