c46d6249675154b023a091724604465679ee0f33
Arthi Sivanantham <arthi_sivanantham@dell.com>
2020-08-11 11:19:14 -0700

    Fix IP address configuration for Management interface

--- CLI/actioner/sonic_cli_if.py
+++ CLI/actioner/sonic_cli_if.py
@@ -231,17 +231,18 @@ def invoke_api(func, args=[]):
 
     elif func == 'patch_mgmt_if_ipv4':
         sp = args[1].split('/')
-        path = cc.Path('/restconf/data/openconfig-interfaces:interfaces/interface={name}/subinterfaces/subinterface={index}/openconfig-if-ip:ipv4/addresses/address={ip}/config', name=args[0], index="0", ip=sp[0])
+        path = cc.Path('/restconf/data/openconfig-interfaces:interfaces/interface={name}/subinterfaces/subinterface={index}/openconfig-if-ip:ipv4/addresses', name=args[0], index="0")
 
-        body = { "openconfig-if-ip:config":  {"ip" : sp[0], "prefix-length" : int(sp[1])} }
+        body = { "openconfig-if-ip:addresses": {"address":[ {"ip":sp[0], "openconfig-if-ip:config":  {"ip" : sp[0], "prefix-length" : int(sp[1])} }]}}
 
         if len(args) > 2 and args[2] == "secondary":
-            body["openconfig-if-ip:config"].update( {"openconfig-interfaces-ext:secondary": True} )
+            body["openconfig-if-ip:addresses"]["address"][0]["openconfig-if-ip:config"].update( {"openconfig-interfaces-ext:secondary": True} )
 
         if len(args) > 3 and args[2] == "gwaddr":
-            body["openconfig-if-ip:config"].update({ "openconfig-interfaces-ext:gw-addr": args[3]} )
+            body["openconfig-if-ip:addresses"]["address"][0]["openconfig-if-ip:config"].update({ "openconfig-interfaces-ext:gw-addr": args[3]} )
 
         return api.patch(path, body)
+
 
     elif func == 'patch_openconfig_if_ip_interfaces_interface_routed_vlan_ipv6_addresses_address_config':
         sp = args[1].split('/')
